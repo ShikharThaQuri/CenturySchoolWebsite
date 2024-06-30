170e4141738967875cf7b84f87b017959ed09319
ShikharThaQuri <shikharchand4321@gmail.com>
2024-06-30 19:35:18 +0545

    extra

--- src/app/dashboard/Add-Photo/deleteImage.tsx
+++ src/app/dashboard/Add-Photo/deleteImage.tsx
@@ -24,7 +24,7 @@ function DeleteImage(props: any) {
   };
 
   return (
-    <div className="absolute top-0 right-0 left-0 bottom-0 bg-[black] opacity-0 transition delay-150 hover:opacity-80">
+    <div className="absolute top-0 right-0 left-0 bottom-0 bg-[black] opacity-0 transition hover:opacity-80">
       <DeleteIcon
         className="mx-[0.5rem] text-[white] text-[2rem] cursor-pointer absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]"
         onClick={deleteButtonHendel}
